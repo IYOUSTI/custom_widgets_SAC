4e2fc387304dfd4b01cfb1cc5622d5535a6138ac
IYOUSTI <122968396+IYOUSTI@users.noreply.github.com>
2024-03-08 17:29:48 +0100

    [FIX] measures in funnel display

--- Funnel/custom-sap-funnel.js
+++ Funnel/custom-sap-funnel.js
@@ -1,77 +1,128 @@
-async render() {
-  await getScriptPromisify(
-      "https://cdn.staticfile.org/echarts/5.3.0/echarts.min.js"
-  );
 
-  if (!this._myDataSource || this._myDataSource.state !== "success") {
-      return;
-  }
-
-  const measures = this._myDataSource.metadata.feeds.measures.values;
-  const data = measures.map((measure) => {
-      return {
-          name: measure.label,
-          value: this._myDataSource.data[0][measure.id].raw // Assuming all measures have the same number of data points
+    var getScriptPromisify = (src) => {
+        return new Promise((resolve) => {
+          $.getScript(src, resolve);
+        });
       };
-  });
+      
+      (function () {
+        const prepared = document.createElement("template");
+        prepared.innerHTML = `
+                <style>
+                </style>
+                <div id="root" style="width: 100%; height: 100%;">
+                </div>
+              `;
+        class Funnel extends HTMLElement {
+          constructor() {
+            super();
+      
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+      
+            this._root = this._shadowRoot.getElementById("root");
+      
+            this._props = {};
+      
+            this.render();
+          }
+      
+          onCustomWidgetResize(width, height) {
+            this.render();
+          }
+      
+          set myDataSource(dataBinding) {
+            this._myDataSource = dataBinding;
+            this.render();
+          }
+      
+          async render() {
+            await getScriptPromisify(
+              "https://cdn.staticfile.org/echarts/5.3.0/echarts.min.js"
+            );
+      
+            if (!this._myDataSource || this._myDataSource.state !== "success") {
+              return;
+            }
+            
+            let dataset = {};
 
-  const myChart = echarts.init(this._root, "wight");
-  const option = {
-      color: ['#D2EFFF', '#A6E0FF', '#89D1FF', '#1990FF', '#0057D2'],
-      title: {
-          text: "",
-      },
-      tooltip: {
-          trigger: "item",
-          formatter: "{a} <br/>{b} : {c}%",
-      },
-      toolbox: {
-          feature: {
-              dataView: { readOnly: false },
-              restore: {},
-              saveAsImage: {},
-          },
-      },
-      legend: {
-          data: measures.map(measure => measure.label),
-      },
-      series: [
-          {
-              name: "Funnel",
-              type: "funnel",
-              left: "10%",
-              top: 60,
-              bottom: 60,
-              width: "80%",
-              min: 0,
-              max: 100,
-              minSize: "0%",
-              maxSize: "100%",
-              sort: "descending",
-              gap: 2,
-              label: {
-                  show: true,
-                  position: "inside",
+            
+
+            const data = this._myDataSource.data.map((data) => {
+              for (let i = 0; i < this._myDataSource.metadata.feeds.measures.values.length; i++) {
+              
+                dataset.push({
+                  value: data[measure].raw, 
+                  name: data[measure].label
+                });
+              }
+
+
+              return dataset;
+            });
+      
+            const myChart = echarts.init(this._root, "wight");
+            const option = {
+              color: ['#D2EFFF', '#A6E0FF', '#89D1FF', '#1990FF', '#0057D2'],
+              title: {
+                text: "",
               },
-              labelLine: {
-                  length: 10,
-                  lineStyle: {
+              tooltip: {
+                trigger: "item",
+                formatter: "{a} <br/>{b} : {c}%",
+              },
+              toolbox: {
+                feature: {
+                  dataView: { readOnly: false },
+                  restore: {},
+                  saveAsImage: {},
+                },
+              },
+              legend: {
+                data: ["Show", "Click", "Visit", "Inquiry", "Order"],
+              },
+              series: [
+                {
+                  name: "Funnel",
+                  type: "funnel",
+                  left: "10%",
+                  top: 60,
+                  bottom: 60,
+                  width: "80%",
+                  min: 0,
+                  max: 100,
+                  minSize: "0%",
+                  maxSize: "100%",
+                  sort: "descending",
+                  gap: 2,
+                  label: {
+                    show: true,
+                    position: "inside",
+                  },
+                  labelLine: {
+                    length: 10,
+                    lineStyle: {
                       width: 1,
                       type: "solid",
+                    },
                   },
-              },
-              itemStyle: {
-                  borderColor: "#fff",
-                  borderWidth: 4,
-              },
-              emphasis: {
-                  label: {
+                  itemStyle: {
+                    borderColor: "#fff",
+                    borderWidth: 4,
+                  },
+                  emphasis: {
+                    label: {
                       fontSize: 20,
+                    },
                   },
-              },
-              data,
-          },
-      ],
-  };
-  myChart.setOption(option);
-}
+                  data,
+                },
+              ],
+            };
+            myChart.setOption(option);
+          }
+        }
+      
+        customElements.define("custom-sap-funnel", Funnel);
+      })();
