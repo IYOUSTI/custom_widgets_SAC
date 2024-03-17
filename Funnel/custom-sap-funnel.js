81bb43e35fecd9fbf7fddf67e4f0a685a12ab5fc
IYOUSTI <122968396+IYOUSTI@users.noreply.github.com>
2024-03-17 15:22:08 +0100

    [FIX] onclick

--- Funnel/custom-sap-funnel.js
+++ Funnel/custom-sap-funnel.js
@@ -1,218 +1,192 @@
+// custom-sap-funnel.js
+
 var getScriptPromisify = (src) => {
   return new Promise((resolve) => {
-      $.getScript(src, resolve);
+    $.getScript(src, resolve);
   });
 };
 
 (function () {
   const prepared = document.createElement("template");
   prepared.innerHTML = `
-      <style>
-      </style>
-      <div id="root" style="width: 100%; height: 100%;">
-      </div>
+    <style>
+    </style>
+    <div id="root" style="width: 100%; height: 100%;">
+    </div>
   `;
   class Funnel extends HTMLElement {
-      constructor() {
-          super();
+    constructor() {
+      super();
 
-          this._shadowRoot = this.attachShadow({ mode: "open" });
-          this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true));
 
-          this._root = this._shadowRoot.getElementById("root");
+      this._root = this._shadowRoot.getElementById("root");
 
-          this._props = {};
+      this._props = {};
 
-          this.render();
+      this.render();
+    }
 
-          this.addEventListener("click", event => {
-            var event = new Event("onClick");
-            console.log('click detected');
-            this.dispatchEvent(event);
-                });
+    onCustomWidgetResize(width, height) {
+      this.render();
+    }
 
+    set myDataSource(dataBinding) {
+      this._myDataSource = dataBinding;
+      this.render();
+    }
+
+    async render() {
+      await getScriptPromisify(
+        "https://cdn.staticfile.org/echarts/5.3.0/echarts.min.js"
+      );
+
+      if (!this._myDataSource || this._myDataSource.state !== "success") {
+        return;
       }
 
-      onCustomWidgetResize(width, height) {
-          this.render();
-      }
+      const abs_values = [];
+      const rel_values = [];
+      const color_measures = [];
+      const dataset = [];
+      const data = this._myDataSource.data.map((data) => {
+        for (let i = 0; i < this._myDataSource.metadata.feeds.measures.values.length; i++) {
+          const measuresKey = `measures_${i}`;
+          const measure = this._myDataSource.metadata.mainStructureMembers[measuresKey];
+          const measureValue = this._myDataSource.metadata.feeds.measures.values[i];
 
-      set myDataSource(dataBinding) {
-          this._myDataSource = dataBinding;
-          this.render();
-      }
+          abs_values.push(Number(data[measureValue].raw));
 
-      async render() {
-          await getScriptPromisify(
-              "https://cdn.staticfile.org/echarts/5.3.0/echarts.min.js"
-          );
-
-          if (!this._myDataSource || this._myDataSource.state !== "success") {
-              return;
+          if (i === 0) {
+            rel_values.push(100);
+          } else {
+            rel_values.push(
+              ((Number(data[measureValue].raw)) / (Number(data[this._myDataSource.metadata.feeds.measures.values[i-1]].raw)) * 100).toFixed(2)
+            );
           }
-          
-          const abs_values = [];
-          const rel_values = [];
-          const color_measures = [];
-          const dataset = [];
-          const data = this._myDataSource.data.map((data) => {
-              for (let i = 0; i < this._myDataSource.metadata.feeds.measures.values.length; i++) {
-                const measuresKey = `measures_${i}`;
-                const measure = this._myDataSource.metadata.mainStructureMembers[measuresKey];
-                const measureValue = this._myDataSource.metadata.feeds.measures.values[i];
 
-                abs_values.push(Number(data[measureValue].raw));
-
-                if (i === 0) {
-                  rel_values.push(100);
-                } else {
-                  rel_values.push(
-                    ((Number(data[measureValue].raw)) / (Number(data[this._myDataSource.metadata.feeds.measures.values[i-1]].raw)) * 100).toFixed(2)
-                  );
-                }
-                
-                switch(i) {
-                  case 0:
-                    color_measures.push('#1E8449');
-                    break;
-                  case 1:
-                    color_measures.push('#1E8449');
-                    break;
-                  case 2:
-                    if (rel_values[i] < 95) {
-                      color_measures.push('#C0392B');
-                    } else {
-                      color_measures.push('#1E8449');
-                    }
-                    break;
-                  case 3:
-                    if (rel_values[i] < 99) {
-                      color_measures.push('#C0392B');
-                    } else {
-                      color_measures.push('#1E8449');
-                    }
-                    break;
-                  case 4:
-                    if (rel_values[i] < 94) {
-                      color_measures.push('#C0392B');
-                    } else {
-                      color_measures.push('#1E8449');
-                    }
-                    break;
-                  case 5:
-                    if (rel_values[i] < 98) {
-                      color_measures.push('#C0392B');
-                    } else {
-                      color_measures.push('#1E8449');
-                    }
-                    break;
-                  default:
-                    break;
-                }
-
-                  dataset.push({
-                      value: Number(data[measureValue].raw),
-                      name: `${measure.label} : ${abs_values[i]} (${rel_values[i]}%)`
-                  });
+          switch(i) {
+            case 0:
+              color_measures.push('#1E8449');
+              break;
+            case 1:
+              color_measures.push('#1E8449');
+              break;
+            case 2:
+              if (rel_values[i] < 95) {
+                color_measures.push('#C0392B');
+              } else {
+                color_measures.push('#1E8449');
               }
-              console.log('dataset');
-              console.log(dataset);
-              console.log('abs_values');
-              console.log(abs_values);
-              console.log('rel_values');
-              console.log(rel_values);
-              console.log('color_measures');
-              console.log(color_measures);
-              return dataset;
+              break;
+            case 3:
+              if (rel_values[i] < 99) {
+                color_measures.push('#C0392B');
+              } else {
+                color_measures.push('#1E8449');
+              }
+              break;
+            case 4:
+              if (rel_values[i] < 94) {
+                color_measures.push('#C0392B');
+              } else {
+                color_measures.push('#1E8449');
+              }
+              break;
+            case 5:
+              if (rel_values[i] < 98) {
+                color_measures.push('#C0392B');
+              } else {
+                color_measures.push('#1E8449');
+              }
+              break;
+            default:
+              break;
+          }
+
+          dataset.push({
+            value: Number(data[measureValue].raw),
+            name: `${measure.label} : ${abs_values[i]} (${rel_values[i]}%)`
           });
+        }
+        return dataset;
+      });
 
-          const myChart = echarts.init(this._root, "wight");
-          const option = {
-              color: color_measures,
-              title: {
-                  text: "",
-              },
-              tooltip: {
-                  trigger: "item",
-                  formatter: function(params) {
-                    const dataIndex = params.dataIndex;
-                    const absValue = abs_values[dataIndex];
-                    const relValue = rel_values[dataIndex];
-                    const delta_abs = dataIndex > 0 ? absValue - abs_values[dataIndex - 1] : 0;
-                
-                    return `▲ : ${delta_abs} (${relValue}%)`;
-                },
-              },
-              toolbox: {
-                  feature: {
-                      dataView: { readOnly: false },
-                      restore: {},
-                      saveAsImage: {},
-                  },
-              },
-              legend: {
-                show: true,
-                formatter: function(name) {
-                    return name.split(':')[0].trim();
-                }
+      const myChart = echarts.init(this._root, "wight");
+      const option = {
+        color: color_measures,
+        title: {
+          text: "",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: function(params) {
+            const dataIndex = params.dataIndex;
+            const absValue = abs_values[dataIndex];
+            const relValue = rel_values[dataIndex];
+            const delta_abs = dataIndex > 0 ? absValue - abs_values[dataIndex - 1] : 0;
+          
+            return `▲ : ${delta_abs} (${relValue}%)`;
+          },
+        },
+        toolbox: {
+          feature: {
+            dataView: { readOnly: false },
+            restore: {},
+            saveAsImage: {},
+          },
+        },
+        legend: {
+          show: true,
+          formatter: function(name) {
+            return name.split(':')[0].trim();
+          }
+        },
+        series: [
+          {
+            name: "Funnel",
+            type: "funnel",
+            left: "10%",
+            top: 60,
+            bottom: 60,
+            width: "80%",
+            minSize: "0%",
+            maxSize: "100%",
+            gap: 5,
+            sort: 'none',
+            label: {
+              show: true,
+              position: "inside",
             },
-              series: [
-                  {
-                      name: "Funnel",
-                      type: "funnel",
-                      left: "10%",
-                      top: 60,
-                      bottom: 60,
-                      width: "80%",
-                      minSize: "0%",
-                      maxSize: "100%",
-                      gap: 5,
-                      sort: 'none',
-                      label: {
-                          show: true,
-                          position: "inside",
-                      },
-                      labelLine: {
-                          length: 10,
-                          lineStyle: {
-                              width: 1,
-                              type: "solid",
-                          },
-                      },
-                      itemStyle: {
-                          borderColor: "#515A5A",
-                          borderWidth: 0.5,
-                      },
-                      emphasis: {
-                          label: {
-                              fontSize: 20,
-                          },
-                      },
-                      data: dataset,
-                  },
-              ],
-          };
-          myChart.setOption(option);
+            labelLine: {
+              length: 10,
+              lineStyle: {
+                width: 1,
+                type: "solid",
+              },
+            },
+            itemStyle: {
+              borderColor: "#515A5A",
+              borderWidth: 0.5,
+            },
+            emphasis: {
+              label: {
+                fontSize: 20,
+              },
+            },
+            data: dataset,
+          },
+        ],
+      };
+      myChart.setOption(option);
 
-          // Event listener for clicking on funnel labels
-          myChart.on('click', function (params) {
-            console.log('Clicked on label:', params.name);
-        });
-
-      }
-
-      onCustomWidgetBeforeUpdate(changedProperties){
-        console.log('changedproperties before');
-        console.log(changedProperties);
-        }
-
-      onCustomWidgetAfterUpdate(changedProperties){
-        console.log('changedproperties after');
-        console.log(changedProperties);
-        }
-
-      
-
+      // Event listener for clicking on funnel labels
+      myChart.on('click', (params) => {
+        const selectedMeasure = params.name.split(':')[0].trim(); // Extract the measure name
+        this.dispatchEvent(new CustomEvent('onClick', { detail: { selectedMeasure } }));
+      });
+    }
   }
 
   customElements.define("custom-sap-funnel", Funnel);
